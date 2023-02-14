1fe4675a8cf00cab991f048581a268a2b8ca9494
saisumanth valluri <saisumanth.valluri@infinitylearn.com>
2023-02-14 16:11:47 +0530

    Project Initialized

---
+++ tasty_kitchens/src/App.js
@@ -0,0 +1,16 @@
+
+import {Component} from 'react'
+import Login from './Components/Login'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return(
+      <>
+        <Login />
+      </>
+    )
+  }
+}
+
+export default App;
