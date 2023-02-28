aab61b4b309977786653b28db41baeaf613b4903
saisumanth valluri <saisumanth.valluri@infinitylearn.com>
2023-02-28 11:10:36 +0530

    Changed Font family

--- tasty_kitchens/src/Components/Header/index.js
+++ tasty_kitchens/src/Components/Header/index.js
@@ -51,7 +51,7 @@ class Header extends Component {
             <div className='nav-bar'>
                 <div className='responsive-box'>
                     <Link to='/' onClick={this.onHome}>
-                        <div style={{display: "flex", flexDirection: "row", alignItems: "center", width: "260px", justifyContent: "space-between"}}>
+                        <div style={{display: "flex", flexDirection: "row", alignItems: "center", width: "280px", justifyContent: "space-between"}}>
                             <img src={appLogo} alt="" className='app-logo' />
                             <h1 className='app-title'>Tasty Kitchens</h1>
                         </div>
